d4c55f1897e56f1db82e2bd38f795a606506662c
Garrett Cox <gcox5714@gmail.com>
2020-12-14 16:11:04 -0500

    hello world server commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var http = require('http');
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 8080;
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello World!');
+});
 
-const helloWorld = () => {
-    console.log('Hello World');
-};
-
-helloWorld();
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
