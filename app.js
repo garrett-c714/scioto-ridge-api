f609a34f402b4317f1350149af5ccae6948a985c
Garrett Cox <gcox5714@gmail.com>
2020-12-17 22:48:48 -0500

    typo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 8080;
+const port = 8000;
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/plain');
     switch (request.url) {
